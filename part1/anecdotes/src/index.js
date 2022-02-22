820299139477cfbb9a89d8245555924c6e0f2d2e
Maicol eduardo Alvarez <maicol_a_18@hotmail.com>
2022-02-21 19:54:38 -0500

    Styles anecdotes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,5 +1,16 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import { Container, Navbar, Modal } from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 const Button = ({onClick, text}) => (
   <button onClick={onClick}>
@@ -7,17 +18,22 @@ const Button = ({onClick, text}) => (
   </button>
 );
 
+const object = {};
+anecdotes.map((a, index) => object[index] = 0)
+
 const App = () => {
 
   const [selected, setSelected] = useState(0)
   const [minVal] = useState(0)
-  const [maxVal] = useState(5)
-  const [clicks, setClicks] = useState(
-    { 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0 }
-  )
-  
+  const [maxVal] = useState(anecdotes.length-1)
+  const [clicks, setClicks] = useState(object)
+
   const handleClick = () => {
-    setSelected(Math.floor(Math.random() * (maxVal - minVal + 1) + minVal));
+    let number = Math.floor(Math.random() * (maxVal - minVal + 1) + minVal)
+    while(number === selected) {
+      number = Math.floor(Math.random() * (maxVal - minVal + 1) + minVal)
+    };
+    setSelected(number);
   };
 
   const handleLeftClick = () => {
@@ -38,28 +54,41 @@ const App = () => {
   };
   
   return (
-    <div>
-      <h1>Anecdote of the day</h1>
-      <p>{anecdotes[selected]}</p>
-      <p>has {clicks[selected]} votes</p>
-      <Button onClick={handleClick} text = "next anecdote"/>
-      <Button onClick={handleLeftClick} text = "votes"/>
-      <h1>Anecdote with most votes</h1>
-      <p>{anecdotes[maxVotesAnacdotes[0]]}</p>
-      <p>has {maxVotesNumber[0]} votes</p>
-    </div>
+    <React.Fragment>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+          <img
+            alt=""
+            src="/Logo.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          Anecdote of the day
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+      <br />
+      <Container>
+        <p>{anecdotes[selected]}</p>
+        <p>has {clicks[selected]} votes</p>
+        <Button onClick={handleClick} text = "next anecdote"/>
+        <Button onClick={handleLeftClick} text = "votes"/>
+        <Modal.Dialog>
+          <Modal.Header >
+            <Modal.Title>Anecdote with most votes</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>{anecdotes[maxVotesAnacdotes[0]]}</p>
+            <p>has {maxVotesNumber[0]} votes</p>
+          </Modal.Body>
+        </Modal.Dialog>
+      </Container>
+    </React.Fragment>
   )
   
 }
-
-const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
 
 ReactDOM.render(
   <App />,
